0001f702bafdc9b33f5c301a9d134859072e206f
pragyabhartispj2007-ops <pragyabhartispj2007@gmail.com>
2026-06-23 14:43:03 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 122345
+let accountEmail = "pragya@google.com"
+var accoubtPassword = "12345"
+accountCity = "Jaipur"
+
+console.log(accountId);
+
+/*
+prefer not to use var because of issue in
+block scope and functional scope
+*/
+
+console.table([accoubtPassword, accountEmail, accountId, accountCity]);
