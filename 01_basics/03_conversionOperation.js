5ce93826eb6bb0c816c5546bdfc523b8a637853a
pragyabhartispj2007-ops <pragyabhartispj2007@gmail.com>
2026-06-27 13:27:55 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,55 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//33 => 33
+//33abc => NaN
+//true => 1 ; false => 0 
+
+let isLoggedIn = "Pragya"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+//1 => true; 0 => false
+//"" => false
+//"Pragya" => true
+
+let someNumber = 33
+stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//**********operations**********
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "hello"
+let str2 = " pragya"
+let str3 = str1 + str2
+//console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
